5611267af812dd4384e1bf19d5f2979c02adb285
ShadyNagy <ShadyNagi@gmail.com>
2022-08-08 15:57:17 +0200

    CountryUiService added.

---
+++ src/ShadyNagy.ApiTemplate.Api/Interfaces/ICountryUiService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ShadyNagy.ApiTemplate.Api.Dtos;
+
+namespace ShadyNagy.ApiTemplate.Api.Interfaces;
+
+public interface ICountryUiService
+{
+  Task<List<CountryDto>> GetCountriesByFilterAsync(CountryFilterDto filter);
+}
---
+++ src/ShadyNagy.ApiTemplate.Api/Services/CountryUiService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using ShadyNagy.ApiTemplate.Api.Dtos;
+using ShadyNagy.ApiTemplate.Api.Interfaces;
+using ShadyNagy.ApiTemplate.Core.Entities;
+using ShadyNagy.ApiTemplate.Core.Specifications;
+using ShadyNagy.ApiTemplate.Core.Specifications.Filters;
+using ShadyNagy.ApiTemplate.SharedKernel.Interfaces;
+
+namespace ShadyNagy.ApiTemplate.Api.Services;
+
+public class CountryUiService : ICountryUiService
+{
+  private readonly IMapper _mapper;
+  private readonly IReadRepository<Country> _countryRepository;
+
+  public CountryUiService(IMapper mapper, IReadRepository<Country> countryRepository) {
+    _mapper = mapper;
+    _countryRepository = countryRepository;
+  }
+
+  public async Task<List<CountryDto>> GetCountriesByFilterAsync(CountryFilterDto filter)
+  {
+    var spec = new CountriesByFilterSpec(_mapper.Map<CountryFilter>(filter));
+    var countries = await _countryRepository.ListAsync(spec);
+
+    return _mapper.Map<List<CountryDto>>(countries);
+  }
+}
