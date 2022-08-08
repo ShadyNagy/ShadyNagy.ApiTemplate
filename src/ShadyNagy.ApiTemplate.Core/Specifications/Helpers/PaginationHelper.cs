93826b9b39dbb9cbb1f7164605900453d8389cfa
ShadyNagy <ShadyNagi@gmail.com>
2022-08-08 15:00:38 +0200

    PaginationHelper added.

---
+++ src/ShadyNagy.ApiTemplate.Core/Specifications/Helpers/PaginationHelper.cs
@@ -0,0 +1,28 @@
+﻿using ShadyNagy.ApiTemplate.Core.Specifications.Filters;
+
+namespace ShadyNagy.ApiTemplate.Core.Specifications.Helpers;
+public static class PaginationHelper
+{
+  public static int DefaultPage => 1;
+  public static int DefaultPageSize => 10;
+
+  public static int CalculateTake(int pageSize)
+  {
+    return pageSize <= 0 ? DefaultPageSize : pageSize;
+  }
+  public static int CalculateSkip(int pageSize, int page)
+  {
+    page = page <= 0 ? DefaultPage : page;
+
+    return CalculateTake(pageSize) * (page - 1);
+  }
+
+  public static int CalculateTake(BaseFilter baseFilter)
+  {
+    return CalculateTake(baseFilter.PageSize);
+  }
+  public static int CalculateSkip(BaseFilter baseFilter)
+  {
+    return CalculateSkip(baseFilter.PageSize, baseFilter.Page);
+  }
+}
